06a97111ebb7931579a6a9d8534b71aacc2e3d03
Jose Augusto <joseaugusto.881@outlook.com>
2025-08-02 11:47:46 -0300

    settings for pipeline, tests coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-require "ccopy"
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+end
+
+require 'ccopy'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
