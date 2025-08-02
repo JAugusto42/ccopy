ed136e1dce3d779fff24b2052b3bfc3a28232ae9
Jose Augusto <joseaugusto.881@outlook.com>
2025-08-02 12:12:35 -0300

    fix specs to test both, linux and mac

--- spec/ccopy_spec.rb
+++ spec/ccopy_spec.rb
@@ -20,39 +20,86 @@ RSpec.describe Ccopy do
         expect { Ccopy::Main.call("--help") }.to output(/Usage/).to_stdout
       end
 
-      it "shows debug info when --debug flag is used" do
-        # Mock stdin to simulate piped input
-        allow($stdin).to receive(:tty?).and_return(false)
-        allow($stdin).to receive(:read).and_return("test")
+      context "on macOS" do
+        before do
+          allow(RbConfig::CONFIG).to receive(:[]).with("host_os").and_return("darwin24")
+        end
 
-        # Mock IO.popen to avoid real system calls
-        allow(IO).to receive(:popen).with("pbcopy", "w").and_yield(double("io", write: nil))
+        it "shows debug info when --debug flag is used" do
+          # Mock stdin to simulate piped input
+          allow($stdin).to receive(:tty?).and_return(false)
+          allow($stdin).to receive(:read).and_return("test")
 
-        expect { Ccopy::Main.call("--debug") }.to output(/Debug/).to_stdout
+          # Mock IO.popen to avoid real system calls
+          allow(IO).to receive(:popen).with("pbcopy", "w").and_yield(double("io", write: nil))
+
+          expect { Ccopy::Main.call("--debug") }.to output(/Debug/).to_stdout
+        end
+
+        it "copies stdin to clipboard when input is piped" do
+          # Mock stdin to simulate piped input
+          allow($stdin).to receive(:tty?).and_return(false)
+          allow($stdin).to receive(:read).and_return("test content")
+
+          # Mock IO.popen to avoid real system calls
+          io_mock = double("io")
+          expect(io_mock).to receive(:write).with("test content")
+          allow(IO).to receive(:popen).with("pbcopy", "w").and_yield(io_mock)
+
+          expect { Ccopy::Main.call }.to output(/Copied to clipboard!/).to_stdout
+        end
+
+        it "handles clipboard errors gracefully" do
+          # Mock stdin to simulate piped input
+          allow($stdin).to receive(:tty?).and_return(false)
+          allow($stdin).to receive(:read).and_return("test")
+
+          # Mock IO.popen to raise an error
+          allow(IO).to receive(:popen).with("pbcopy", "w").and_raise(StandardError.new("Clipboard error"))
+
+          expect { Ccopy::Main.call }.to output(/Error copying to clipboard: Clipboard error/).to_stdout
+        end
       end
 
-      it "copies stdin to clipboard when input is piped" do
-        # Mock stdin to simulate piped input
-        allow($stdin).to receive(:tty?).and_return(false)
-        allow($stdin).to receive(:read).and_return("test content")
+      context "on Linux" do
+        before do
+          allow(RbConfig::CONFIG).to receive(:[]).with("host_os").and_return("linux-gnu")
+        end
 
-        # Mock IO.popen to avoid real system calls
-        io_mock = double("io")
-        expect(io_mock).to receive(:write).with("test content")
-        allow(IO).to receive(:popen).with("pbcopy", "w").and_yield(io_mock)
+        it "shows debug info when --debug flag is used" do
+          # Mock stdin to simulate piped input
+          allow($stdin).to receive(:tty?).and_return(false)
+          allow($stdin).to receive(:read).and_return("test")
 
-        expect { Ccopy::Main.call }.to output(/Copied to clipboard!/).to_stdout
-      end
+          # Mock IO.popen to avoid real system calls
+          allow(IO).to receive(:popen).with("xclip -selection clipboard", "w").and_yield(double("io", write: nil))
 
-      it "handles clipboard errors gracefully" do
-        # Mock stdin to simulate piped input
-        allow($stdin).to receive(:tty?).and_return(false)
-        allow($stdin).to receive(:read).and_return("test")
+          expect { Ccopy::Main.call("--debug") }.to output(/Debug/).to_stdout
+        end
 
-        # Mock IO.popen to raise an error
-        allow(IO).to receive(:popen).and_raise(StandardError.new("Clipboard error"))
+        it "copies stdin to clipboard when input is piped" do
+          # Mock stdin to simulate piped input
+          allow($stdin).to receive(:tty?).and_return(false)
+          allow($stdin).to receive(:read).and_return("test content")
 
-        expect { Ccopy::Main.call }.to output(/Error copying to clipboard: Clipboard error/).to_stdout
+          # Mock IO.popen to avoid real system calls
+          io_mock = double("io")
+          expect(io_mock).to receive(:write).with("test content")
+          allow(IO).to receive(:popen).with("xclip -selection clipboard", "w").and_yield(io_mock)
+
+          expect { Ccopy::Main.call }.to output(/Copied to clipboard!/).to_stdout
+        end
+
+        it "handles clipboard errors gracefully" do
+          # Mock stdin to simulate piped input
+          allow($stdin).to receive(:tty?).and_return(false)
+          allow($stdin).to receive(:read).and_return("test")
+
+          # Mock IO.popen to raise an error
+          allow(IO).to receive(:popen).with("xclip -selection clipboard", "w").and_raise(StandardError.new("Clipboard error"))
+
+          expect { Ccopy::Main.call }.to output(/Error copying to clipboard: Clipboard error/).to_stdout
+        end
       end
     end
   end
